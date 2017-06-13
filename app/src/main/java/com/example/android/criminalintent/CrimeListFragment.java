18a7f24c1a90c3916f327669e7d80cb455ae526c
home <907813456@qq.com>
2017-06-13 23:08:25 +0800

    add

--- app/src/main/java/com/example/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/android/criminalintent/CrimeListFragment.java
@@ -1,10 +1,60 @@
 package com.example.android.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by wangchao on 2017/6/13.
  */
 
 public class CrimeListFragment extends Fragment {
+
+    private RecyclerView mCrimeRecyclerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+
+        mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+
+    }
+
+    private class CrimeHolder extends RecyclerView.ViewHolder {
+
+        public TextView mTitleTextView;
+
+        public CrimeHolder(View itemView) {
+            super(itemView);
+
+            mTitleTextView = (TextView) itemView;
+        }
+
+    }
+
+    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
+
+        private List<Crime> mCrimes;
+
+        public CrimeAdapter(List<Crime> crimes) {
+            mCrimes = crimes;
+        }
+
+        @Override
+        public CrimeHolder omCreateViewHolder() {
+
+        }
+
+    }
+
+
 }
